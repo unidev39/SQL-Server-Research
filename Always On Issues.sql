bb1f268ec1d57bb78dd6f754fb26792c92ca2ca6
suman pantha <hrtpantha@gmail.com>
2018-03-05 14:02:41 +0545

    Update Always On Issues.sql

--- Always On Issues.sql
+++ Always On Issues.sql
@@ -4,3 +4,5 @@
 --Wizard looks ta the exact same folder path. Here is the query to find the exact path
 
 SELECT DB_NAME(database_id), physical_name FROM SYS.MASTER_FILES
+
+--Reference: https://blog.sqlauthority.com/2017/11/23/sql-server-alwayson-wizard-error-checking-compatibility-database-file-locations-server-instance-hosts-secondary-replica/
