a8b3fcfb964ae81fac779a561b320faf30c15ca6
suman pantha <hrtpantha@gmail.com>
2018-03-05 14:00:37 +0545

    Create Always On Issues.sql

---
+++ Always On Issues.sql
@@ -0,0 +1,6 @@
+
+Checking for compatibility of the database file locations on the server instance that hosts secondary replica WIN-KOB3V8MHTBK\VMSQLSERVER (Microsoft.SqlServer.Management.HadrModel)
+
+Wizard looks ta the exact same folder path. Here is the query to find the exact path
+
+SELECT DB_NAME(database_id), physical_name FROM SYS.MASTER_FILES
