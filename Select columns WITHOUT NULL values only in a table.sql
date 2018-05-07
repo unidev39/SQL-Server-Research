b2669aa14cd6a3a800367a0a1a6a8b43c1ff2190
suman pantha <hrtpantha@gmail.com>
2018-05-07 11:19:29 +0545

    Update Select columns WITHOUT NULL values only in a table.sql

--- Select columns WITHOUT NULL values only in a table.sql
+++ Select columns WITHOUT NULL values only in a table.sql
@@ -29,15 +29,17 @@ CREATE PROC [dbo].[spCustomerHistory]
 AS 
 BEGIN 
 SET NOCOUNT ON 
---EXEC spCustomerHistoryCursor  @UserId= '44D143EA-ABC3-4335-A5C4-0001336C7D3E';
+--EXEC spCustomerHistory  @UserId= '44D143EA-ABC3-4335-A5C4-0001336C7D3E';
 
 IF OBJECT_ID('Tempdb..#Result', 'U') IS NOT NULL
 DROP TABLE #Result;
 
-SELECT  COLUMN_NAME, CAST(NULL AS NVARCHAR(MAX)) AS OldValue, CAST(NULL AS NVARCHAR(MAX)) AS NewValue
+SELECT  COLUMN_NAME
 INTO  #Result
 FROM  INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME = 'CustomerHistory' AND COLUMN_NAME NOT IN ( 'UserId','RefCategoryId','RefSubCategoryId','ModifiedById')
+WHERE TABLE_NAME = 'CustomerHistory' AND COLUMN_NAME NOT IN ( 'Id','UserId','RefCategoryId','RefSubCategoryId','ModifiedById')
+
+ALTER TABLE #Result ADD OldValue VARCHAR(MAX), NewValue VARCHAR(MAX) 
 
 DECLARE @ColumnName VARCHAR(255),
         @SQLSelect VARCHAR(MAX),
@@ -56,39 +58,39 @@ BEGIN
      DROP TABLE #TempResult;
 
      SET @SQLSelect = N'';     
-     SELECT @SQLSelect += ('SELECT '''+ @ColumnName + ''' AS colname,
+     SELECT @SQLSelect += ('SELECT TOP 1 '''+ @ColumnName + ''' AS colname,
                                   ch.'+ @ColumnName + ' AS Old'+@ColumnName+', 
-                            	  c.' + @ColumnName + ' AS New'+@ColumnName + '
+                               c.' + @ColumnName + ' AS New'+@ColumnName + '
                             FROM Customer c 
                             JOIN CustomerHistory ch on c.UserId = ch.UserId 
-                            WHERE c.UserId = ''' + CAST(@UserId AS NVARCHAR(36)) +'''');
-     					    
+                            WHERE c.UserId = ''' + CAST(@UserId AS NVARCHAR(36)) +'''ORDER BY ch.Id desc');
+              
      CREATE TABLE #TempResult (ColumnsName varchar(max) NULL,NewValue varchar(max), OldValue varchar(max));
      INSERT INTO #TempResult EXEC (@SQLSelect);
      
-     UPDATE R
+      UPDATE R
      SET  R.COLUMN_NAME = CASE WHEN ISNULL(TR.NewValue,'') = ISNULL(TR.OldValue,'') THEN NULL 
 						       WHEN RIGHT(@ColumnName,2) = 'Id' THEN SUBSTRING(@ColumnName,1,(len(@ColumnName)-2))						   
 	                      ELSE R.COLUMN_NAME END,
-     	  R.NewValue = CASE WHEN @ColumnName = 'CustomerStatus'      THEN ISNULL((SELECT Status FROM CustomerStatus WHERE Id = TR.NewValue ),'N/A')     	                        						
-     						WHEN @ColumnName = 'OccupationId'        THEN ISNULL((SELECT CodeDescription FROM Occupation WHERE Id = TR.NewValue ),'N/A')
+     	  R.NewValue = CASE WHEN @ColumnName = 'CustomerStatus'      THEN (SELECT Status FROM CustomerStatus WHERE Id = TR.NewValue)
+     						WHEN @ColumnName = 'OccupationId'        THEN (SELECT CodeDescription FROM Occupation WHERE Id = TR.NewValue)
      						WHEN @ColumnName = 'ResidentialStatusId' THEN CASE WHEN TR.NewValue = 1 THEN 'Non-Malaysian' ELSE 'Malaysian' END
-							WHEN @ColumnName = 'OfficeStateMalId'    THEN ISNULL((SELECT MalState FROM StateMalaysia WHERE Id = TR.NewValue ),'N/A')
-							WHEN @ColumnName = 'SourceOfWealthId'    THEN ISNULL((SELECT SourceDescription FROM SourceOfWealth WHERE Id = TR.NewValue ),'N/A')							
-							WHEN @ColumnName = 'StateId'             THEN ISNULL((SELECT StateName FROM State WHERE Id = TR.NewValue),'N/A')
+							WHEN @ColumnName = 'OfficeStateMalId'    THEN (SELECT MalState FROM StateMalaysia WHERE Id = TR.NewValue )
+							WHEN @ColumnName = 'SourceOfWealthId'    THEN (SELECT SourceDescription FROM SourceOfWealth WHERE Id = TR.NewValue )
+							WHEN @ColumnName = 'StateId'             THEN (SELECT StateName FROM State WHERE Id = TR.NewValue)
 							WHEN @ColumnName = 'KYCType'             THEN CASE WHEN TR.NewValue = 0 THEN 'NONKYC' WHEN TR.NewValue = 1 THEN 'KYC' ELSE 'EKYC' END
-							WHEN @ColumnName IN ( 'NationalityId', 'CountryOfBirthId', 'PaymentCountryId') THEN ISNULL((SELECT CountryName FROM Country WHERE Id = TR.NewValue ),'N/A')							
-     	               ELSE  ISNULL(TR.NewValue, 'N/A') END, 
+							WHEN @ColumnName IN ( 'NationalityId', 'CountryOfBirthId', 'PaymentCountryId') THEN (SELECT CountryName FROM Country WHERE Id = TR.NewValue )
+     	               ELSE  TR.NewValue END, 
 					       
-     	  R.OldValue = CASE WHEN @ColumnName = 'CustomerStatus'      THEN ISNULL((SELECT Status FROM CustomerStatus WHERE Id = TR.OldValue ), 'N/A')     						
-     						WHEN @ColumnName = 'OccupationId'        THEN ISNULL((SELECT CodeDescription FROM Occupation WHERE Id = TR.OldValue ), 'N/A')
+     	  R.OldValue = CASE WHEN @ColumnName = 'CustomerStatus'      THEN (SELECT Status FROM CustomerStatus WHERE Id = TR.OldValue )
+     						WHEN @ColumnName = 'OccupationId'        THEN (SELECT CodeDescription FROM Occupation WHERE Id = TR.OldValue )
      						WHEN @ColumnName = 'ResidentialStatusId' THEN CASE WHEN TR.OldValue = 1 THEN 'Non-Malaysian' ELSE 'Malaysian' END 							
-							WHEN @ColumnName = 'OfficeStateMalId'    THEN ISNULL((SELECT MalState FROM StateMalaysia WHERE Id = TR.OldValue ),'N/A')
-							WHEN @ColumnName = 'SourceOfWealthId'    THEN ISNULL((SELECT SourceDescription FROM SourceOfWealth WHERE Id = TR.OldValue ),'N/A')							
-							WHEN @ColumnName = 'StateId'             THEN ISNULL((SELECT StateName FROM State WHERE Id = TR.OldValue),'N/A')
+							WHEN @ColumnName = 'OfficeStateMalId'    THEN (SELECT MalState FROM StateMalaysia WHERE Id = TR.OldValue )
+							WHEN @ColumnName = 'SourceOfWealthId'    THEN (SELECT SourceDescription FROM SourceOfWealth WHERE Id = TR.OldValue )
+							WHEN @ColumnName = 'StateId'             THEN (SELECT StateName FROM State WHERE Id = TR.OldValue)
 							WHEN @ColumnName = 'KYCType'             THEN CASE WHEN TR.OldValue = 0 THEN 'NONKYC' WHEN TR.OldValue = 1 THEN 'KYC' ELSE 'EKYC' END
-							WHEN @ColumnName IN ( 'NationalityId', 'CountryOfBirthId', 'PaymentCountryId') THEN ISNULL((SELECT CountryName FROM Country WHERE Id = TR.OldValue ),'N/A')  
-     				   ELSE ISNULL(TR.OldValue,'N/A') END
+							WHEN @ColumnName IN ( 'NationalityId', 'CountryOfBirthId', 'PaymentCountryId') THEN (SELECT CountryName FROM Country WHERE Id = TR.OldValue )
+     				   ELSE TR.OldValue END
      FROM 
           #Result R
      JOIN #TempResult TR ON R.COLUMN_NAME = TR.ColumnsName;
@@ -98,7 +100,11 @@ END;
 END;
 CLOSE dbCursor;
 DEALLOCATE dbCursor;
-SELECT * FROM #Result WHERE COLUMN_NAME is not null;
+SELECT  
+     COLUMN_NAME AS ColumnName, 
+	 CASE WHEN OldValue = '' THEN 'N/A' ELSE ISNULL(OldValue, 'N/A') END AS [From],
+	 CASE WHEN NewValue = '' THEN 'N/A' ELSE ISNULL(NewValue,'N/A') END AS [To]
+FROM #Result WHERE COLUMN_NAME is not null;
 DROP TABLE #Result;
 DROP TABLE #TempResult;
 END;
